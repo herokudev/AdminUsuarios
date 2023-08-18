9dc0ac2695278361e40aaa900bd160916c6bfa12
herokudev <ao43085@gmail.com>
2023-08-18 17:51:30 -0600

    first commit

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+    public function index()
+    {
+        return Usuario::all();
+    }
+
+    public function getById(int $id)
+    {
+        return Usuario::find($id);
+    }
+}
---
+++ database/seeders/UsuarioSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Usuario;
+
+class UsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Usuario::factory(25)->create();
+    }
+}
